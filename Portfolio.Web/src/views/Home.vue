4be66846df8b658167161e2f2a186c20e98d7a2b
Ryan Golden <65426263+Xionxiii@users.noreply.github.com>
2023-02-22 00:15:36 -0800

    deleted hello world

--- Portfolio.Web/src/views/Home.vue
+++ Portfolio.Web/src/views/Home.vue
@@ -2,7 +2,6 @@
   <div class="home">
     <!-- Component is auto-imported by `unplugin-vue-components`
     (either kebab-case or PascalCase works.) -->
-    <HelloWorld msg="Welcome to your new Vue.js Coalesce application!" />
   </div>
 </template>
 
